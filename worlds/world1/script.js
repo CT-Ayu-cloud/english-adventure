1c7adcb01e4d97bdbdbfe584aba8c7387e070452
CT-Ayu-cloud <g-62131673@moe-dl.edu.my>
2026-07-12 15:38:34 +0800

    world 1 scene test

--- worlds/world1/script.js
+++ worlds/world1/script.js
@@ -1,31 +1,32 @@
+class World1 extends Phaser.Scene {
+
+    constructor() {
+        super("World1");
+    }
+
+    create() {
+
+        this.add.text(
+            400,
+            80,
+            "ANIMAL ISLAND",
+            {
+                font: "48px Arial",
+                fill: "#ffffff"
+            }
+        );
+
+    }
+
+}
+
 const config = {
     type: Phaser.AUTO,
-
     parent: "game",
-
     width: 1280,
     height: 720,
-
     backgroundColor: "#87CEEB",
-
-    scene: {
-        create: create
-    }
+    scene: World1
 };
 
 new Phaser.Game(config);
-
-function create(){
-
-    this.add.text(
-        380,
-        60,
-        "🌴 ANIMAL ISLAND",
-        {
-            fontSize: "48px",
-            color: "#ffffff",
-            fontStyle: "bold"
-        }
-    );
-
-}
