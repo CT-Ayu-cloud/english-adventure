d7211693ec83f88b74b792ad75029199c4cc8cf9
CT-Ayu-cloud <g-62131673@moe-dl.edu.my>
2026-07-12 16:07:54 +0800

    World1 Step3 Player Movement

--- worlds/world1/script.js
+++ worlds/world1/script.js
@@ -1,4 +1,41 @@
-document.body.innerHTML = `
-<h1 style="color:white;font-size:60px;text-align:center;margin-top:100px;">
-SCRIPT BERJALAN
-</h1>`;
+const player = document.getElementById("player");
+
+let x = 120;
+let y = 500;
+
+const speed = 6;
+
+document.addEventListener("keydown", function(e){
+
+    switch(e.key){
+
+        case "ArrowUp":
+        case "w":
+        case "W":
+            y -= speed;
+            break;
+
+        case "ArrowDown":
+        case "s":
+        case "S":
+            y += speed;
+            break;
+
+        case "ArrowLeft":
+        case "a":
+        case "A":
+            x -= speed;
+            break;
+
+        case "ArrowRight":
+        case "d":
+        case "D":
+            x += speed;
+            break;
+
+    }
+
+    player.style.left = x + "px";
+    player.style.top = y + "px";
+
+});
