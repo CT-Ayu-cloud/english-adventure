64bf5be65eb020ce3bc479ea5f607306ed6ed810
CT-Ayu-cloud <g-62131673@moe-dl.edu.my>
2026-07-12 14:50:29 +0800

    Game Hub V2

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const buttons = document.querySelectorAll(".menuBtn");
 
+buttons[0].addEventListener("click", function () {
+
+    alert("🏝️ Welcome to Animal Island!\n\nWorld 1 is under construction.");
+
+});
+
+buttons[1].addEventListener("click", function () {
+
+    alert("🍎 Food Forest\nComing Soon");
+
+});
+
+buttons[2].addEventListener("click", function () {
+
+    alert("🎨 Colour Cave\nComing Soon");
+
+});
+
+buttons[3].addEventListener("click", function () {
+
+    alert("📖 Reading Kingdom\nComing Soon");
+
+});
+
+buttons[4].addEventListener("click", function () {
+
+    alert("🏆 Achievement System\nComing Soon");
+
+});
