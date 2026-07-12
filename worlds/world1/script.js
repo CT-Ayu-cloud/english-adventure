c7a7d5e67f79de8c6006407d8992c5d9a993b38f
CT-Ayu-cloud <g-62131673@moe-dl.edu.my>
2026-07-12 15:05:13 +0800

    Player Movement V1

--- worlds/world1/script.js
+++ worlds/world1/script.js
@@ -1,1 +1,45 @@
+const player = document.getElementById("player");
 
+let x = 100;
+let y = 420;
+
+const speed = 10;
+
+function updatePlayer(){
+
+    player.style.left = x + "px";
+    player.style.top = y + "px";
+
+}
+
+document.addEventListener("keydown", function(e){
+
+    switch(e.key){
+
+        case "ArrowLeft":
+            x -= speed;
+            break;
+
+        case "ArrowRight":
+            x += speed;
+            break;
+
+        case "ArrowUp":
+            y -= speed;
+            break;
+
+        case "ArrowDown":
+            y += speed;
+            break;
+
+    }
+
+    // Had sempadan skrin
+    x = Math.max(0, Math.min(window.innerWidth - 60, x));
+    y = Math.max(0, Math.min(window.innerHeight - 60, y));
+
+    updatePlayer();
+
+});
+
+updatePlayer();
